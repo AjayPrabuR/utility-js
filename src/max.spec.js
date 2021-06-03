cca0fed5520e6316388ad03e90dcdf93153d5d05
ajayprabur <ajayprabur@gmail.com>
2021-06-03 22:09:47 +0530

    Added edge test cases for max function

--- src/max.spec.js
+++ src/max.spec.js
@@ -10,4 +10,12 @@ describe('Max', () => {
         expect(max([])).toEqual(undefined);
     });
 
+    it('Max of non-empty array with mix of negative numbers is maximum element in the array', () => {
+        expect(max([-1,2,-3,4,-5])).toEqual(4);
+    });
+
+    it('Max of non-empty array with only negative numbers is maximum element in the array', () => {
+        expect(max([-1,-2,-3,-4,-5])).toEqual(-1);
+    });
+
 })
